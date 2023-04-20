120d4696865c0625ce7398837131b7a1ca4a7122
steffengroenandersen <steffengroenandersen@gmail.com>
2023-04-20 11:24:39 +0200

    First commit

---
+++ src/main/java/com/thegiftgarden/TheGiftGardenApplication.java
@@ -0,0 +1,13 @@
+package com.thegiftgarden;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheGiftGardenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheGiftGardenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thegiftgarden/TheGiftGardenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thegiftgarden;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheGiftGardenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
