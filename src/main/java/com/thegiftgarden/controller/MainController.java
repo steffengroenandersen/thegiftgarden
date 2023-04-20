5d11c06d943dcb5badf9da13c541bfed2ac6d18c
Steffen Grøn Andersen <steffengroenandersen@gmail.com>
2023-04-20 21:30:23 +0200

    Add MainController, User and UserRepo

--- src/main/java/com/thegiftgarden/controller/MainController.java
+++ src/main/java/com/thegiftgarden/controller/MainController.java
@@ -1,10 +1,18 @@
 package com.thegiftgarden.controller;
 
+import com.thegiftgarden.model.User;
+import com.thegiftgarden.repository.UserRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MainController {
+
+    // REPOSITORIES
+    UserRepository userRepository;
+
 
     // HOMEPAGE
     @GetMapping("/")
@@ -23,5 +31,30 @@ public class MainController {
     public String createAccount(){
         return "createaccount";
     }
+
+    @PostMapping("/createuser")
+    public String createUser(
+            @RequestParam("firstName") String newFirstName,
+            @RequestParam("lastName") String newLastName,
+            @RequestParam("email") String newEmail,
+            @RequestParam("password") String newPassword){
+
+         User newUser = new User();
+
+         newUser.setFirstName(newFirstName);
+         newUser.setLastName(newLastName);
+         newUser.setEmail(newEmail);
+         newUser.setPassword(newPassword);
+
+         userRepository.addUser(newUser);
+
+
+
+
+        return "redirect:/login";
+    }
+
+
+
 
 }
---
+++ src/main/java/com/thegiftgarden/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.thegiftgarden.repository;
+
+import com.thegiftgarden.model.User;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class UserRepository {
+
+    // ADD NEW USERS TO DATABASE
+    public void addUser(User newUser){
+        
+    }
+}
